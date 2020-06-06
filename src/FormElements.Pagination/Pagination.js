1d9fba31795b42ae0871e214005537a57c97a292
grzdrz <53759907+grzdrz@users.noreply.github.com>
2020-06-07 00:17:35 +0700

    В пагинации добавил стрелкам обработчики и исправил реакцию многоточий на клики по ним.

--- src/FormElements.Pagination/Pagination.js
+++ src/FormElements.Pagination/Pagination.js
@@ -8,18 +8,22 @@
 export function paginationScript() {
     let getPaginationByPugCode = require("./paginationForJSRender.pug");
     {
-        let curPage = 1;
-
-        function setClickEventsToLinks() {
+        function setClickEventsToPagesLinks() {
             let paginations = document.querySelectorAll(".pagination");
             paginations.forEach(e => {
                 let links = e.querySelectorAll(".pagination__pageLink");
                 links.forEach(ee => {
                     ee.onclick = selectPage;
-                })
+                });
+
+                let leftArrow = e.querySelector(".pagination__leftArrow");
+                if (leftArrow) leftArrow.onclick = selectPage;
+
+                let rightArrow = e.querySelector(".pagination__rightArrow");
+                if (rightArrow) rightArrow.onclick = selectPage;
             });
         }
-        setClickEventsToLinks();
+        setClickEventsToPagesLinks();
 
         function selectPage(event) {
             event.preventDefault();
@@ -27,17 +31,37 @@ export function paginationScript() {
             /* потенциальный асинхронный запрос на сервер данных для требуемой страницы */
             /* if(запрос успешен? === false) return; */
 
-            curPage = Number.parseInt(event.currentTarget.textContent);
-            event.currentTarget.parentElement.parentElement.innerHTML = getPaginationByPugCode({
-                pagesCount: 20,
-                curPage: curPage,
-            });
-
+            let selectedPageNumber = event.currentTarget.textContent;
+            if (selectedPageNumber === "arrow_forward") {
+                let classes = event.currentTarget.className.split(/\s/i);
+                if (classes.includes("pagination__leftArrow")) {
+                    let prevPage = --(event.currentTarget.parentElement.dataset.curPageNumber);
+                    event.currentTarget.parentElement.parentElement.innerHTML = getPaginationByPugCode({
+                        pagesCount: 20,
+                        curPageNumber: prevPage,
+                    });
+                }
+                else if (classes.includes("pagination__rightArrow")) {
+                    let nextPage = ++(event.currentTarget.parentElement.dataset.curPageNumber);
+                    event.currentTarget.parentElement.parentElement.innerHTML = getPaginationByPugCode({
+                        pagesCount: 20,
+                        curPageNumber: nextPage,
+                    });
+                }
+            }
+            else if (selectedPageNumber === "...") return;
+            else {
+                event.currentTarget.parentElement.dataset.curPageNumber = Number.parseInt(selectedPageNumber);
+                event.currentTarget.parentElement.parentElement.innerHTML = getPaginationByPugCode({
+                    pagesCount: 20,
+                    curPageNumber: Number.parseInt(selectedPageNumber),
+                });
+            }
             //переустановка ивентов на обновленный объект пагинации, т.к. элемент был полностью пересоздан.
             //P.S. Данный метод переустанавливает обработчики для ивентов ВСЕХ пагинаций на странице, т.к. 
             //после перерендера элемента соответственно пропадает и ссылка на него через event.currentTarget...
             //Плюс т.к. подразумевается что на одной странице одна пагинация, то теоретически это допустимо.
-            setClickEventsToLinks();
+            setClickEventsToPagesLinks();
         }
     }
 }
