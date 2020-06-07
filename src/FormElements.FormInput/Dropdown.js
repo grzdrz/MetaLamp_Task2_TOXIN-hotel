b0c948144687350b08665d4227edd53b25607651
grzdrz <53759907+grzdrz@users.noreply.github.com>
2020-06-07 16:55:39 +0700

    Отрефакторил дропдаун. Добавил в него события выпадения/исчезновения при перемещении мыши над ним.

--- src/FormElements.FormInput/Dropdown.js
+++ src/FormElements.FormInput/Dropdown.js
@@ -1,79 +1,108 @@
 export function dropdownScript() {
-    function onDropdown(event) {
-        let targetDropdown = event.currentTarget.parentElement;
-        let dropwdownList = targetDropdown.querySelector(".formInput__dropedList");
-
-        if (dropwdownList.style.display === "none") {
-            targetDropdown.style.borderBottomLeftRadius = "0px";
-            targetDropdown.style.borderBottomRightRadius = "0px";
-            dropwdownList.style.display = "flex";
-        }
-        else {
-            targetDropdown.style.borderBottomLeftRadius = "4px";
-            targetDropdown.style.borderBottomRightRadius = "4px";
-            dropwdownList.style.display = "none";
-        }
-    }
-
-    function onDropdownItemPlus(event) {
-        let curValue = Number.parseInt(event.currentTarget.parentElement.querySelector("p").textContent);
-        curValue++;
-        if (curValue !== 0) event.currentTarget.parentElement.querySelector(".formInput__dropedListItemHandlerMinus").style.opacity = 1;
-        event.currentTarget.parentElement.querySelector("p").textContent = curValue.toString();
-
-        changeDropdownInputValue(event);
-    }
-    function onDropdownItemMinus(event) {
-        let curValue = Number.parseInt(event.currentTarget.parentElement.querySelector("p").textContent);
-        curValue--;
-        if (curValue < 0) curValue = 0;
-        if (curValue === 0) event.currentTarget.style.opacity = 0.38;
-        event.currentTarget.parentElement.querySelector("p").textContent = curValue.toString();
-
-        changeDropdownInputValue(event);
-    }
-
-    function changeDropdownInputValue(event) {
-        let curDropdownElement = event.currentTarget.parentElement.parentElement.parentElement.parentElement;
-        let dropdownList = curDropdownElement.querySelectorAll(".formInput__dropedListItem");
-        let sum = Array.from(dropdownList).reduce((sum, e) => {
-            return sum + Number.parseInt(e.querySelector(".formInput__dropedListItemHandler > p").textContent)
-        }, 0);
-
-        //склонение слова гость по числу
-        let test = curDropdownElement.className.match(/formInput__dropdown_fullNumber/i);
-        let test2 = curDropdownElement.className.match(/formInput__dropdown_listOfNumbers/i);
-        if (curDropdownElement.className.match(/formInput__dropdown_fullNumber/i)) {
-            if (sum.toString()[sum.toString().length - 1] === "1" && sum !== 11)
-                curDropdownElement.parentElement.querySelector(".formInput__mainInput").value = sum + " гость";
-            else if ((sum.toString()[sum.toString().length - 1] > 1 && sum.toString()[sum.toString().length - 1] <= 4) &&
-                (sum < 12 || sum > 14))
-                curDropdownElement.parentElement.querySelector(".formInput__mainInput").value = sum + " гостя";
-            else
-                curDropdownElement.parentElement.querySelector(".formInput__mainInput").value = sum + " гостей";
-        }
-        else if (curDropdownElement.className.match(/formInput__dropdown_listOfNumbers/i)) {
-            let allItemNames = Array.from(curDropdownElement.querySelectorAll(".formInput__dropedListItem"));
-            let result = allItemNames.reduce((fullString, e, index) => {
-                let itemName = e.querySelector("p").textContent.toLowerCase();
-                let itemValue = e.querySelector(".formInput__dropedListItemHandler > p").textContent;
-                if (index !== allItemNames.length - 1)
-                    return fullString + itemValue + " " + itemName + ", ";
-                else
-                    return fullString + itemValue + " " + itemName;
-            }, "")
-            curDropdownElement.parentElement.querySelector(".formInput__mainInput").value = result;
-        }
-    }
 
     let dropdownsElems = document.querySelectorAll(".formInput_dropdown");
     dropdownsElems.forEach(e => {
         e.querySelector(".formInput__dropdownInput").onclick = onDropdown;
+        e.querySelector(".formInput__dropdownInput").onmouseenter = onDropdownEnter;
+        e.querySelector(".formInput__dropedList").onmouseleave = onDropdownLeave;
+
         e.querySelectorAll(".formInput__dropedListItemHandlerPlus").forEach(e => {
             e.onclick = onDropdownItemPlus;
         });
         e.querySelectorAll(".formInput__dropedListItemHandlerMinus").forEach(e => {
             e.onclick = onDropdownItemMinus;
         });
+
+        let inputContainer = e.querySelector(".formInput__input");
+        inputContainer.dataset.borderRadius = inputContainer.style.borderTopLeftRadius;
     });
+
+    
+    function onDropdown(event) {
+        let inputContainer = event.currentTarget.parentElement;
+        let dropwdownList = inputContainer.querySelector(".formInput__dropedList");
+
+        if (dropwdownList.style.display === "none") {
+            inputContainer.style.borderBottomLeftRadius = "0px";
+            inputContainer.style.borderBottomRightRadius = "0px";
+            dropwdownList.style.display = "flex";
+        }
+        else {
+            inputContainer.style.borderBottomLeftRadius = inputContainer.dataset.borderRadius;
+            inputContainer.style.borderBottomRightRadius = inputContainer.dataset.borderRadius;
+            dropwdownList.style.display = "none";
+        }
+    }
+    function onDropdownEnter(event) {
+        let inputContainer = event.currentTarget.parentElement;
+        let dropwdownList = inputContainer.querySelector(".formInput__dropedList");
+
+        if (dropwdownList.style.display === "none") {
+            inputContainer.style.borderBottomLeftRadius = "0px";
+            inputContainer.style.borderBottomRightRadius = "0px";
+            dropwdownList.style.display = "flex";
+        }
+        else return;
+    }
+    function onDropdownLeave(event) {
+        let inputContainer = event.currentTarget.parentElement;
+
+        inputContainer.style.borderBottomLeftRadius = inputContainer.dataset.borderRadius;
+        inputContainer.style.borderBottomRightRadius = inputContainer.dataset.borderRadius;
+        event.currentTarget.style.display = "none";
+    }
+
+
+    function onDropdownItemPlus(event) {
+        let dropedListItemHandlerText = event.currentTarget.parentElement.querySelector(".formInput__dropedListItemHandlerText");
+        let curValue = Number.parseInt(dropedListItemHandlerText.textContent);
+        curValue++;
+        if (curValue !== 0)
+            event.currentTarget.parentElement.querySelector(".formInput__dropedListItemHandlerMinus").style.opacity = 1;
+        dropedListItemHandlerText.textContent = curValue.toString();
+
+        changeDropdownInputValue(event);
+    }
+    function onDropdownItemMinus(event) {
+        let dropedListItemHandlerText = event.currentTarget.parentElement.querySelector(".formInput__dropedListItemHandlerText");
+        let curValue = Number.parseInt(dropedListItemHandlerText.textContent);
+        curValue--;
+        if (curValue < 0) curValue = 0;
+        if (curValue === 0)
+            event.currentTarget.style.opacity = 0.38;
+        dropedListItemHandlerText.textContent = curValue.toString();
+
+        changeDropdownInputValue(event);
+    }
+
+    function changeDropdownInputValue(event) {
+        let curDropdownContainer = event.currentTarget.parentElement.parentElement.parentElement.parentElement;
+        let dropdownList = curDropdownContainer.querySelectorAll(".formInput__dropedListItem");
+        let sum = Array.from(dropdownList).reduce((sum, e) => {
+            return sum + Number.parseInt(e.querySelector(".formInput__dropedListItemHandlerText").textContent)
+        }, 0);
+
+        //склонение слова гость по числу
+        if (curDropdownContainer.className.match(/formInput__dropdown_sumNumber/i)) {
+            if (sum.toString()[sum.toString().length - 1] === "1" && sum !== 11)
+                curDropdownContainer.parentElement.querySelector(".formInput__mainInput").value = sum + " гость";
+            else if ((sum.toString()[sum.toString().length - 1] > 1 && sum.toString()[sum.toString().length - 1] <= 4) &&
+                (sum < 12 || sum > 14))
+                curDropdownContainer.parentElement.querySelector(".formInput__mainInput").value = sum + " гостя";
+            else
+                curDropdownContainer.parentElement.querySelector(".formInput__mainInput").value = sum + " гостей";
+        }
+        else if (curDropdownContainer.className.match(/formInput__dropdown_listOfNumbers/i)) {
+            let allItemsNames = Array.from(curDropdownContainer.querySelectorAll(".formInput__dropedListItem"));
+            let result = allItemsNames.reduce((fullString, e, index) => {
+                let itemName = e.querySelector("p").textContent;
+                let itemValue = e.querySelector(".formInput__dropedListItemHandlerText").textContent;
+                if (index !== allItemsNames.length - 1)
+                    return fullString + itemValue + " " + itemName + ", ";
+                else
+                    return fullString + itemValue + " " + itemName;
+            }, "");
+            curDropdownContainer.parentElement.querySelector(".formInput__mainInput").value = result;
+        }
+    }
 }
