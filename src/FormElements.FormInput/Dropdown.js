1ca46142680a0ddb1682b40ceaf33fce4c5164ca
grzdrz <53759907+grzdrz@users.noreply.github.com>
2020-06-08 22:37:00 +0700

    Добавил кнопки очистить и добавить в дропдаун. Поправил выпадение дропдауна при наведении на соответствующий инпут.

--- src/FormElements.FormInput/Dropdown.js
+++ src/FormElements.FormInput/Dropdown.js
@@ -2,9 +2,13 @@ export function dropdownScript() {
 
     let dropdownsElems = document.querySelectorAll(".formInput_dropdown");
     dropdownsElems.forEach(e => {
-        e.querySelector(".formInput__dropdownInput").onclick = onDropdown;
-        e.querySelector(".formInput__dropdownInput").onmouseenter = onDropdownEnter;
-        e.querySelector(".formInput__dropedList").onmouseleave = onDropdownLeave;
+        e.querySelector(".formInput__mainInput").onclick = onDropdownClick;
+        e.querySelector(".formInput__dropdownInput").onclick = onDropdownClick;
+        //если обработчик выше повесить на контейнер, то дропдаун будет закрываться при клике по нему, т.к. находится в контейнере
+
+        let inputContainer = e.querySelector(".formInput__input");
+        inputContainer.onmouseenter = onDropdownEnter;
+        inputContainer.onmouseleave = onDropdownLeave;
 
         e.querySelectorAll(".formInput__dropedListItemHandlerPlus").forEach(e => {
             e.onclick = onDropdownItemPlus;
@@ -13,12 +17,22 @@ export function dropdownScript() {
             e.onclick = onDropdownItemMinus;
         });
 
-        let inputContainer = e.querySelector(".formInput__input");
+        let clearButton = e.querySelector(".formInput__clearButton");
+        if (clearButton) {
+            clearButton.disabled = true;
+            clearButton.style.opacity = 0;
+            clearButton.onclick = onClear;
+        }
+        let applyButton = e.querySelector(".formInput__applyButton");
+        if (applyButton) {
+            applyButton.onclick = onApply;
+        }
+
         inputContainer.dataset.borderRadius = inputContainer.style.borderTopLeftRadius;
     });
 
-    
-    function onDropdown(event) {
+
+    function onDropdownClick(event) {
         let inputContainer = event.currentTarget.parentElement;
         let dropwdownList = inputContainer.querySelector(".formInput__dropedList");
 
@@ -34,7 +48,7 @@ export function dropdownScript() {
         }
     }
     function onDropdownEnter(event) {
-        let inputContainer = event.currentTarget.parentElement;
+        let inputContainer = event.currentTarget;
         let dropwdownList = inputContainer.querySelector(".formInput__dropedList");
 
         if (dropwdownList.style.display === "none") {
@@ -42,18 +56,21 @@ export function dropdownScript() {
             inputContainer.style.borderBottomRightRadius = "0px";
             dropwdownList.style.display = "flex";
         }
-        else return;
     }
     function onDropdownLeave(event) {
-        let inputContainer = event.currentTarget.parentElement;
+        let inputContainer = event.currentTarget;
+        let dropwdownList = inputContainer.querySelector(".formInput__dropedList");
 
-        inputContainer.style.borderBottomLeftRadius = inputContainer.dataset.borderRadius;
-        inputContainer.style.borderBottomRightRadius = inputContainer.dataset.borderRadius;
-        event.currentTarget.style.display = "none";
+        if (dropwdownList.style.display === "flex") {
+            inputContainer.style.borderBottomLeftRadius = inputContainer.dataset.borderRadius;
+            inputContainer.style.borderBottomRightRadius = inputContainer.dataset.borderRadius;
+            dropwdownList.style.display = "none";
+        }
     }
 
 
     function onDropdownItemPlus(event) {
+        event.preventDefault();
         let dropedListItemHandlerText = event.currentTarget.parentElement.querySelector(".formInput__dropedListItemHandlerText");
         let curValue = Number.parseInt(dropedListItemHandlerText.textContent);
         curValue++;
@@ -61,22 +78,38 @@ export function dropdownScript() {
             event.currentTarget.parentElement.querySelector(".formInput__dropedListItemHandlerMinus").style.opacity = 1;
         dropedListItemHandlerText.textContent = curValue.toString();
 
-        changeDropdownInputValue(event);
+        let listContainer = event.currentTarget.parentElement.parentElement.parentElement;
+        let clearButton = listContainer.querySelector(".formInput__clearButton");
+        if (clearButton) {
+            clearButton.disabled = false;
+            clearButton.style.opacity = 1;
+        }
+
+        changeDropdownInputValue(event.currentTarget.parentElement.parentElement.parentElement.parentElement);
     }
     function onDropdownItemMinus(event) {
+        event.preventDefault();
         let dropedListItemHandlerText = event.currentTarget.parentElement.querySelector(".formInput__dropedListItemHandlerText");
         let curValue = Number.parseInt(dropedListItemHandlerText.textContent);
         curValue--;
         if (curValue < 0) curValue = 0;
-        if (curValue === 0)
+        if (curValue === 0) {
             event.currentTarget.style.opacity = 0.38;
+
+            let listContainer = event.currentTarget.parentElement.parentElement.parentElement;
+            let clearButton = listContainer.querySelector(".formInput__clearButton");
+            if (clearButton) {
+                clearButton.disabled = true;
+                clearButton.style.opacity = 0;
+            }
+        }
         dropedListItemHandlerText.textContent = curValue.toString();
 
-        changeDropdownInputValue(event);
+        changeDropdownInputValue(event.currentTarget.parentElement.parentElement.parentElement.parentElement);
     }
 
-    function changeDropdownInputValue(event) {
-        let curDropdownContainer = event.currentTarget.parentElement.parentElement.parentElement.parentElement;
+    function changeDropdownInputValue(curDropdownContainer) {
+        //let curDropdownContainer = event.currentTarget.parentElement.parentElement.parentElement.parentElement;
         let dropdownList = curDropdownContainer.querySelectorAll(".formInput__dropedListItem");
         let sum = Array.from(dropdownList).reduce((sum, e) => {
             return sum + Number.parseInt(e.querySelector(".formInput__dropedListItemHandlerText").textContent)
@@ -104,5 +137,30 @@ export function dropdownScript() {
             }, "");
             curDropdownContainer.parentElement.querySelector(".formInput__mainInput").value = result;
         }
+    }
+
+    function onClear(event) {
+        event.currentTarget.disabled = true;
+        event.currentTarget.style.opacity = 0;
+
+        let inputContainer = event.currentTarget.parentElement.parentElement.parentElement;
+        inputContainer.querySelector(".formInput__mainInput").value = inputContainer.dataset.defaultValue;
+        inputContainer.querySelectorAll(".formInput__dropedListItemHandlerText").forEach(e => {
+            e.textContent = "0";
+        });
+        inputContainer.querySelectorAll(".formInput__dropedListItemHandlerMinus").forEach(e => {
+            e.style.opacity = 0.38;
+        });
+    }
+
+    function onApply(event) {
+        let inputContainer = event.currentTarget.parentElement.parentElement.parentElement;
+        let dropwdownList = inputContainer.querySelector(".formInput__dropedList");
+
+        inputContainer.style.borderBottomLeftRadius = inputContainer.dataset.borderRadius;
+        inputContainer.style.borderBottomRightRadius = inputContainer.dataset.borderRadius;
+        dropwdownList.style.display = "none";
+
+        changeDropdownInputValue(inputContainer);
     }
 }
