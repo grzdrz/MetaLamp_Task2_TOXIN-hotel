13b37aad2dd3dfaf705b61220213a3de37e83446
grzdrz <53759907+grzdrz@users.noreply.github.com>
2020-06-13 05:29:10 +0700

    Добавил цветовые блоки на макет ColorsAndType.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const WebpackConfig = require("./webpackconfigBuilder");
 
-let fileNames = ["HeadersAndFooters", "FormElements"];
+let fileNames = [
+    /* "HeadersAndFooters",
+    "FormElements", */
+    "ColorsAndType"];
 
 module.exports = fileNames.map(fileName => {
     return new WebpackConfig(fileName, __dirname);
