3b51a8af369ba79704b003b85c2dfdb72c41e66a
grzdrz <53759907+grzdrz@users.noreply.github.com>
2020-08-03 09:13:30 +0700

    Доверстал range-slider.

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -7,17 +7,36 @@ global.jQuery = $;
 global.$ = $;
 
 class RangeSlider {
-    constructor(oterContainerElement) {
-        this.containerElement = oterContainerElement;
+    constructor(outerContainerElement) {
+        this.containerElement = outerContainerElement.querySelector(".range-slider");
+        this.content = this.containerElement.querySelector(".range-slider__content");
+        this.currentValues = this.containerElement.querySelector(".range-slider__current-values");
 
-        this.jqSlider = $(this.containerElement).ionRangeSlider({
+        this.renderTitle = this.renderTitle.bind(this);
+
+        this.data = {
             type: "double",
             min: 0,
-            max: 1000,
-            from: 200,
-            to: 500,
-            grid: true,
-        });
+            max: 15000,
+            from: 5000,
+            to: 10000,
+            step: 100,
+            hide_min_max: true,
+            hide_from_to: true,
+            onStart: this.renderTitle,
+            onUpdate: this.renderTitle,
+            onChange: this.renderTitle,
+        };
+
+        this.jqSlider = $(this.content).ionRangeSlider(this.data);
+    }
+
+    renderTitle(data) {
+        this.currentValues.textContent = `${this.formateNumber(data.from)}₽ - ${this.formateNumber(data.to)}₽`;
+    }
+
+    formateNumber(number) {
+        return number.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1 ");
     }
 }
 
