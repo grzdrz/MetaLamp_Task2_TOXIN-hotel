b5a37c53ed0982bc3524028a436b040af7e84a52
Nikita Khakimov <53759907+grzdrz@users.noreply.github.com>
2020-08-16 16:15:47 +0700

    Поменял ховер/неховер цвета местами. Заодно пофиксил выпадение дропдауна и подсветку троеточия в пагинации #8

--- src/components/dropdown/list-item.js
+++ src/components/dropdown/list-item.js
@@ -25,7 +25,7 @@ class ListItem {
         let curValue = Number.parseInt(this.value.textContent, 10);
         curValue += 1;
         if (curValue !== 0) this.minus.style.opacity = 1;
-        this.value.textContent = curValue.toString();
+        this.value.textContent = curValue;
 
         if (this.dropdown.clearButton) {
             this.dropdown.clearButton.style.display = "flex";
@@ -41,7 +41,7 @@ class ListItem {
         curValue -= 1;
         if (curValue < 0) curValue = 0;
 
-        this.value.textContent = curValue.toString();
+        this.value.textContent = curValue;
 
         if (curValue === 0) {
             event.currentTarget.style.opacity = 0.38;
