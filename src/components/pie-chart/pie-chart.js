318ecb42b6c0a68679db0452a89197c98914e114
Nikita Khakimov <53759907+grzdrz@users.noreply.github.com>
2020-08-22 11:40:16 +0700

    Заменил нумерацию в стар рейтинге на семантическое перечисление через nth-of-type #14

--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,22 +1,22 @@
-import ApexCharts from "apexcharts";
+import ApexCharts from 'apexcharts';
 
 class PieChart {
     constructor(outerContainerElement) {
         this.outerContainerElement = outerContainerElement;
         this.chart = {};
 
-        this.initialize();
+        this._initialize();
     }
 
-    initialize() {
-        this.containerElement = this.outerContainerElement.querySelector(".pie-chart .pie-chart__pie");
+    _initialize() {
+        this.containerElement = this.outerContainerElement.querySelector('.pie-chart .pie-chart__pie');
 
         const options = {
             chart: {
                 offsetX: 0,
-                type: "donut",
-                height: "150px",
-                width: "150px",
+                type: 'donut',
+                height: '150px',
+                width: '150px',
             },
             dataLabels: {
                 enabled: false,
@@ -25,19 +25,19 @@ class PieChart {
                 show: false,
             },
             series: [25, 25, 50, 0],
-            labels: ["Великолепно", "Хорошо", "Удовлетворительно", "Разочарован"],
+            labels: ['Великолепно', 'Хорошо', 'Удовлетворительно', 'Разочарован'],
             plotOptions: {
                 pie: {
                     donut: {
-                        size: "112px",
+                        size: '112px',
                     },
                 },
             },
-            colors: ["#BC9CFF", "#6FCF97", "#FFE39C", "#919191"],
+            colors: ['#BC9CFF', '#6FCF97', '#FFE39C', '#919191'],
             fill: {
-                type: "gradient",
+                type: 'gradient',
                 gradient: {
-                    gradientToColors: ["#8BA4F9", "#66D2EA", "#FFBA9C", "#3D4975"],
+                    gradientToColors: ['#8BA4F9', '#66D2EA', '#FFBA9C', '#3D4975'],
                     stops: [80, 100],
                 },
             },
