36015247710d27f39dec8b7715fc3fd5983778ad
Nikita Khakimov <53759907+grzdrz@users.noreply.github.com>
2020-09-08 23:52:32 +0700

    Доверстал бургер. Вставил ссылки на соответствующие страницы в кнопки хедера. #8

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,19 +10,22 @@ class Header {
   _initialize() {
     this.logoContainer = this.container.querySelector('.js-header__colored-logo');
     this.burger = this.container.querySelector('.js-header__burger');
+    this.burgerElements = Array.from(this.burger.querySelectorAll('.js-header__burger-element'));
     this.navigationMenu = this.container.querySelector('.js-header__navigation-menu');
     this.buttons = this.container.querySelector('.js-header__buttons');
     this.userFullName = this.container.querySelector('.js-header__user-full-name');
 
     this.burger.addEventListener('click', this._handleBurgerClick);
+    this._handleBurgerClick();
   }
 
   _handleBurgerClick() {
-    this.logoContainer.classList.toggle('header__colored-logo_opened');
-    this.burger.classList.toggle('header__burger_opened');
-    this.navigationMenu.classList.toggle('header__navigation-menu_opened');
-    if (this.buttons) this.buttons.classList.toggle('header__buttons_opened');
-    if (this.userFullName) this.userFullName.classList.toggle('header__user-full-name_opened');
+    this.logoContainer.classList.toggle('header__colored-logo_closed');
+    this.burger.classList.toggle('header__burger_closed');
+    this.burgerElements.forEach((element) => element.classList.toggle('header__burger-element_opened'));
+    this.navigationMenu.classList.toggle('header__navigation-menu_closed');
+    if (this.buttons) this.buttons.classList.toggle('header__buttons_closed');
+    if (this.userFullName) this.userFullName.classList.toggle('header__user-full-name_closed');
   }
 }
 
