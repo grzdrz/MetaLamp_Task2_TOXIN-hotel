46a5690a1b42f0c2af7e58dcad330a9e844e4f30
Nikita Khakimov <53759907+grzdrz@users.noreply.github.com>
2020-10-24 09:03:12 +0700

    Вынес функцию склонения слов по числу в хелперы

--- src/components/price-calculation-form/price-calculation-form.js
+++ src/components/price-calculation-form/price-calculation-form.js
@@ -1,5 +1,6 @@
 import Dropdown from '../dropdown/dropdown';
 import Calendar from '../calendar/calendar';
+import { doDeclensionOfWord } from '../../assets/helpers';
 
 class PriceCalculationForm {
   constructor(outerContainerElement) {
@@ -81,7 +82,7 @@ class PriceCalculationForm {
   }
 
   _render() {
-    this.mainSumFormula.textContent = `${this._formateNumber(this.roomRentalPrice)}${this.currencyType} x ${this.daysCount} ${this._doDeclensionOfWord(this.daysCount)}`;
+    this.mainSumFormula.textContent = `${this._formateNumber(this.roomRentalPrice)}${this.currencyType} x ${this.daysCount} ${doDeclensionOfWord(this.daysCount, ['сутки', 'суток', 'суток'])}`;
     this.mainSumValue.textContent = `${this._formateNumber(this.roomRentalSum)}${this.currencyType}`;
     this.additionalServicesValue.textContent = `${this._formateNumber(this.servicesSum)}${this.currencyType}`;
     this.totalResultValue.textContent = `${this._formateNumber(this.totalSum)}${this.currencyType}`;
@@ -89,23 +90,6 @@ class PriceCalculationForm {
 
   _formateNumber(number) {
     return number.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ');
-  }
-
-  _doDeclensionOfWord(number) {
-    const words = ['сутки', 'суток', 'суток'];
-
-    const stringifiedNumber = number.toString();
-    const isEndOnOne = stringifiedNumber[stringifiedNumber.length - 1] === '1';
-    const isNotEqualEleven = number !== 11;
-    if (isEndOnOne && isNotEqualEleven) return words[0];
-
-    const isEndNumberMoreThenOne = stringifiedNumber[stringifiedNumber.length - 1] > 1;
-    const isEndNumberLessThenFour = stringifiedNumber[stringifiedNumber.length - 1] <= 4;
-    const isEndNumberBetweenTwelveAndFourteen = number < 12 || number > 14;
-    const isSecondWord = isEndNumberMoreThenOne && isEndNumberLessThenFour && isEndNumberBetweenTwelveAndFourteen;
-    if (isSecondWord) return words[1];
-
-    return words[2];
   }
 
   _handleDropdownMinusPlusButtonsClick() {
